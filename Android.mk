3af5c68abad010d8c0cceb713dee6314c4e6f906
Alexander Martinz <eviscerationls@gmail.com>
2015-05-07 10:37:08 +0200

    add required setup for jitpack
    Change-Id: I1fd4a2c6913773bb089cca9115af5223c9e28f2d
    Signed-off-by: Alexander Martinz <eviscerationls@gmail.com>

--- Android.mk
+++ Android.mk
@@ -3,6 +3,6 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE    := customLogger
-LOCAL_SRC_FILES := $(call all-java-files-under,src/main/java)
+LOCAL_SRC_FILES := $(call all-java-files-under,library/src/main/java)
 
 include $(BUILD_STATIC_JAVA_LIBRARY)
