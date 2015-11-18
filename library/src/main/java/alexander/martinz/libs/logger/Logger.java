09b0ed8b7434025e995c94faf6a02aceba4004d3
Alexander Martinz <eviscerationls@gmail.com>
2015-11-18 17:19:27 +0100

    Add source file lookup
      * if enabled, it will add the file name and line number,
        which also allows navigation via android studio log output
    
    Signed-off-by: Alexander Martinz <eviscerationls@gmail.com>

--- library/src/main/java/alexander/martinz/libs/logger/Logger.java
+++ library/src/main/java/alexander/martinz/libs/logger/Logger.java
@@ -27,7 +27,26 @@ public class Logger {
     /**
      * If disabled nothing will happen at all
      */
-    private static boolean DEBUG = false;
+    private static boolean SHOULD_LOG = BuildConfig.DEBUG;
+
+    /**
+     * If disabled, the stack trace lookup will be skipped
+     */
+    private static boolean LOOKUP_DISABLED = false;
+
+    public static final int LOOKUP_TYPE_FULL = 0;
+    public static final int LOOKUP_TYPE_SHORT = 1;
+
+    private static int LOOKUP_TYPE = LOOKUP_TYPE_SHORT;
+
+    public static final char VERBOSE = 'v';
+    public static final char DEBUG = 'd';
+    public static final char INFO = 'i';
+    public static final char WARN = 'w';
+    public static final char ERROR = 'e';
+    public static final char WTF = '?';
+
+    private static final Object[] NULL_OBJECTS = new Object[0];
 
     /**
      * Enables or disables logging. If disabled all operations will not do anything and return early.
@@ -35,86 +54,192 @@ public class Logger {
      * @param enable True to enable, false to disable the logger.
      */
     public static synchronized void setEnabled(final boolean enable) {
-        DEBUG = enable;
+        SHOULD_LOG = enable;
     }
 
     /**
      * @return Whether the logger is enabled.
      */
     public static boolean getEnabled() {
-        return DEBUG;
+        return SHOULD_LOG;
+    }
+
+    /**
+     * Enables or disables stack trace lookups.<p>
+     * When enabled the log line will contain the class name and line number of the method call. <p>
+     *
+     * @param disableLookup True to disable, false to enable.
+     */
+    public static synchronized void setLookupDisabled(final boolean disableLookup) {
+        LOOKUP_DISABLED = disableLookup;
+    }
+
+    /**
+     * @return Whether stack trace lookup is enabled or not.
+     */
+    public static boolean isLookupDisabled() {
+        return LOOKUP_DISABLED;
+    }
+
+    public static synchronized void setLookupType(final int lookupType) {
+        LOOKUP_TYPE = lookupType;
+    }
+
+    public static int getLookupType() {
+        return LOOKUP_TYPE;
     }
 
     public static void d(final Object object, final String msg) {
-        if (DEBUG) Log.d(getTag(object), getMessage(msg));
+        d(object, msg, NULL_OBJECTS);
     }
 
     public static void d(final Object object, final String msg, final Object... objects) {
-        if (DEBUG) Log.d(getTag(object), getMessage(msg, objects));
+        if (SHOULD_LOG) {
+            log(DEBUG, getTag(object), getMessage(msg, objects));
+        }
     }
 
     public static void d(final Object object, final String msg, final Exception exception) {
-        if (DEBUG) Log.d(getTag(object), getMessage(msg), exception);
+        if (SHOULD_LOG) {
+            log(DEBUG, getTag(object), getMessage(msg), exception);
+        }
     }
 
     public static void e(final Object object, final String msg) {
-        if (DEBUG) Log.e(getTag(object), getMessage(msg));
+        e(getTag(object), getMessage(msg), NULL_OBJECTS);
     }
 
     public static void e(final Object object, final String msg, final Object... objects) {
-        if (DEBUG) Log.e(getTag(object), getMessage(msg, objects));
+        if (SHOULD_LOG) {
+            log(ERROR, getTag(object), getMessage(msg, objects));
+        }
     }
 
     public static void e(final Object object, final String msg, final Exception exception) {
-        if (DEBUG) Log.e(getTag(object), getMessage(msg), exception);
+        if (SHOULD_LOG) {
+            log(ERROR, getTag(object), getMessage(msg), exception);
+        }
     }
 
     public static void i(final Object object, final String msg) {
-        if (DEBUG) Log.i(getTag(object), getMessage(msg));
+        i(getTag(object), getMessage(msg), NULL_OBJECTS);
     }
 
     public static void i(final Object object, final String msg, final Object... objects) {
-        if (DEBUG) Log.i(getTag(object), getMessage(msg, objects));
+        if (SHOULD_LOG) {
+            log(INFO, getTag(object), getMessage(msg, objects));
+        }
     }
 
     public static void i(final Object object, final String msg, final Exception exception) {
-        if (DEBUG) Log.i(getTag(object), getMessage(msg), exception);
+        if (SHOULD_LOG) {
+            log(INFO, getTag(object), getMessage(msg), exception);
+        }
     }
 
     public static void v(final Object object, final String msg) {
-        if (DEBUG) Log.v(getTag(object), getMessage(msg));
+        v(getTag(object), getMessage(msg), NULL_OBJECTS);
     }
 
     public static void v(final Object object, final String msg, final Object... objects) {
-        if (DEBUG) Log.v(getTag(object), getMessage(msg, objects));
+        if (SHOULD_LOG) {
+            log(VERBOSE, getTag(object), getMessage(msg, objects));
+        }
     }
 
     public static void v(final Object object, final String msg, final Exception exception) {
-        if (DEBUG) Log.v(getTag(object), getMessage(msg), exception);
+        if (SHOULD_LOG) {
+            log(VERBOSE, getTag(object), getMessage(msg), exception);
+        }
     }
 
     public static void w(final Object object, final String msg) {
-        if (DEBUG) Log.w(getTag(object), getMessage(msg));
+        w(getTag(object), getMessage(msg), NULL_OBJECTS);
     }
 
     public static void w(final Object object, final String msg, final Object... objects) {
-        if (DEBUG) Log.w(getTag(object), getMessage(msg, objects));
+        if (SHOULD_LOG) {
+            log(WARN, getTag(object), getMessage(msg, objects));
+        }
     }
 
     public static void w(final Object object, final String msg, final Exception exception) {
-        if (DEBUG) Log.w(getTag(object), getMessage(msg), exception);
+        if (SHOULD_LOG) {
+            log(WARN, getTag(object), getMessage(msg), exception);
+        }
     }
 
     public static void wtf(final Object object, final String msg) {
-        if (DEBUG) Log.wtf(getTag(object), getMessage(msg));
+        wtf(getTag(object), getMessage(msg), NULL_OBJECTS);
     }
 
     public static void wtf(final Object object, final String msg, final Object... objects) {
-        if (DEBUG) Log.wtf(getTag(object), getMessage(msg, objects));
+        if (SHOULD_LOG) {
+            log(WTF, getTag(object), getMessage(msg, objects));
+        }
     }
 
     public static void wtf(final Object object, final String msg, final Exception exception) {
-        if (DEBUG) Log.wtf(getTag(object), getMessage(msg), exception);
+        if (SHOULD_LOG) {
+            log(WTF, getTag(object), getMessage(msg), exception);
+        }
+    }
+
+    public static void log(final char type, final String tag, final String msg) {
+        log(type, tag, msg, null);
+    }
+
+    public static void log(final char type, final String tag, final String msg, final Exception exception) {
+        switch (type) {
+            case VERBOSE: {
+                if (exception != null) {
+                    Log.v(tag, msg, exception);
+                } else {
+                    Log.v(tag, msg);
+                }
+                break;
+            }
+            case DEBUG: {
+                if (exception != null) {
+                    Log.d(tag, msg, exception);
+                } else {
+                    Log.d(tag, msg);
+                }
+                break;
+            }
+            case INFO: {
+                if (exception != null) {
+                    Log.i(tag, msg, exception);
+                } else {
+                    Log.i(tag, msg);
+                }
+                break;
+            }
+            case WARN: {
+                if (exception != null) {
+                    Log.w(tag, msg, exception);
+                } else {
+                    Log.w(tag, msg);
+                }
+                break;
+            }
+            case ERROR: {
+                if (exception != null) {
+                    Log.e(tag, msg, exception);
+                } else {
+                    Log.e(tag, msg);
+                }
+                break;
+            }
+            case WTF: {
+                if (exception != null) {
+                    Log.wtf(tag, msg, exception);
+                } else {
+                    Log.wtf(tag, msg);
+                }
+                break;
+            }
+        }
     }
 
     /**
@@ -129,18 +254,51 @@ public class Logger {
         if (object instanceof String) {
             return ((String) object);
         } else if (object == null) {
-            return "Logger";
+            if (LOOKUP_DISABLED) {
+                return "Logger";
+            }
+            final StackTraceElement stackTraceElement = getCurrentStackTrace();
+            return ((stackTraceElement != null) ? stackTraceElement.getClassName() : "Logger");
         } else {
             return object.getClass().getSimpleName();
         }
     }
 
-    public static String getMessage(final String msg) {
-        return String.format("--> %s", msg);
+    public static String getMessage(String msg, final Object... objects) {
+        if (objects != null && objects.length > 0 && objects[0] != null) {
+            msg = String.format(msg, objects);
+        }
+        final String sourceFileLocation = (LOOKUP_DISABLED ? "" : getSourceFileLocation());
+        return String.format("%s --> %s", sourceFileLocation, msg);
     }
 
-    public static String getMessage(final String msg, final Object... objects) {
-        return String.format("--> %s", String.format(msg, objects));
+    private static StackTraceElement getCurrentStackTrace() {
+        final StackTraceElement[] stackTraceElements = Thread.currentThread().getStackTrace();
+        if (stackTraceElements == null || stackTraceElements.length < 6) {
+            return null;
+        }
+
+        return stackTraceElements[6];
+    }
+
+    public static String getSourceFileLocation() {
+        final StackTraceElement stackTrace = getCurrentStackTrace();
+        if (stackTrace == null) {
+            return "";
+        }
+        final String className = stackTrace.getFileName();
+        final String methodName = stackTrace.getMethodName();
+        final int lineNumber = stackTrace.getLineNumber();
+
+        final StringBuilder sb = new StringBuilder();
+        if (LOOKUP_TYPE == LOOKUP_TYPE_FULL) {
+            sb.append('[');
+        }
+        sb.append('(').append(className).append(':').append(lineNumber).append(')');
+        if (LOOKUP_TYPE == LOOKUP_TYPE_FULL) {
+            sb.append('#').append(methodName).append(']');
+        }
+        return sb.toString();
     }
 
     /**
